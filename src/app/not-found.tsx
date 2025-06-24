6365e3ad33b3f4f6481f1940f7fc5751199dd60a
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-06-24 17:35:43 +0200

    fix(not-found): improve dark mode compatibility and avoid hydration mismatch

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,26 +1,34 @@
 import MainHeaderContainer from '@/components/containers/mainHeaderContainer/MainHeaderContainer'
+import { useTheme } from '@/hooks/useTheme'
+import { headers } from 'next/headers'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function NotFound() {
-  return (
-    <MainHeaderContainer bluredBackground>
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h1 className="sr-only">Not Found</h1>
-        <h2>Oups...</h2>
-        <Image
-          src="/img/not-found.jpg"
-          alt="Not found"
-          width={256}
-          height={256}
-          className="rounded-xl"
-        />
-        <p className="text-center">Could not find requested resource</p>
-        <Link className="flex gap-3 custom-button" href="/">
-          <span>←</span>
-          Return Home
-        </Link>
-      </div>
-    </MainHeaderContainer>
-  )
+export default async function NotFound() {
+
+    const headersList = await headers()
+    const theme = headersList.get('x-theme')
+    const isDark = theme === 'dark'
+
+    return (
+        <MainHeaderContainer bluredBackground>
+            <div className="flex flex-col justify-center items-center gap-4">
+                <h1 className="sr-only">Not Found</h1>
+                <h2>Oups...</h2>
+                <Image
+                    src={isDark ? "/img/not-found-dark.jpg" : "/img/not-found.jpg"}
+                    alt="Not found"
+                    width={256}
+                    height={256}
+                    className="rounded-xl"
+                    priority
+                />
+                <p className="text-center">Could not find requested resource</p>
+                <Link className="flex gap-3 custom-button" href="/">
+                    <span>←</span>
+                    Return Home
+                </Link>
+            </div>
+        </MainHeaderContainer>
+    )
 }
