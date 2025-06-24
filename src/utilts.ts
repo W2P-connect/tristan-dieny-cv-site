2afe12dd5a5e8edae9eb8f4034933b3f1e330a69
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-06-24 12:20:44 +0200

    refactor(utils): reintroduce utility functions in a cleaner path

---
+++ src/utilts.ts
@@ -0,0 +1,32 @@
+export function clx(
+  ...inputs: Array<string | undefined | false | null | 0 | Record<string, boolean> | string[]>
+): string {
+  return inputs
+    .flatMap((input) => {
+      if (typeof input === 'string') return input
+      if (Array.isArray(input)) return input
+      if (typeof input === 'object' && input !== null) {
+        return Object.entries(input)
+          .filter(([_, value]) => Boolean(value))
+          .map(([key]) => key)
+      }
+      return []
+    })
+    .filter(Boolean)
+    .join(' ')
+}
+
+export const getURL = (path: string = '', id?: string) => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL && process.env.NEXT_PUBLIC_SITE_URL.trim() !== ''
+      ? process.env.NEXT_PUBLIC_SITE_URL
+      : process?.env?.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL.trim() !== ''
+        ? process.env.NEXT_PUBLIC_VERCEL_URL
+        : 'http://localhost:3100/'
+
+  url = url.replace(/\/+$/, '')
+  url = url.includes('http') ? url : `https://${url}`
+  path = path.replace(/^\/+/, '')
+
+  return path ? `${url}${id ? `#${id}` : ''}/${path}` : url
+}
