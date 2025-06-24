d6e6d82adc867f6ebfb574a1bf983c244675bf92
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-06-24 17:35:41 +0200

    feat(theme): retrieve theme from cookies in layout and middleware for SSR support

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,23 @@ import { NextResponse, NextRequest } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+
+  const theme = request.cookies.get("tdcv-theme");
+
+  const headers = new Headers(request.headers);
+  if (theme) {
+    headers.set("x-theme", theme.value);
+  }
+
   if (request.nextUrl.pathname === '/') {
     return NextResponse.redirect(new URL('/en', request.url))
   }
 
-  return NextResponse.next()
+  return NextResponse.next({
+    request: {
+      headers: headers
+    }
+  });
 }
 
 export const config = {
