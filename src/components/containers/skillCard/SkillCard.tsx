a778b89fbd32b055702ab5f8da917eedd9bf4893
Tristan Dieny <tristandieny@Mac.lan>
2025-06-26 17:46:45 +0200

    feat(skill): ajout de la possibilité d'afficher les détails des compétences

--- src/components/containers/skillCard/SkillCard.tsx
+++ src/components/containers/skillCard/SkillCard.tsx
@@ -1,32 +1,61 @@
+import { SkillCardType } from '@/lib/skills'
 import { clx } from '@/utilts'
 import React from 'react'
+import RenderIf from '../renderIf/RenderIf'
+import { Locale } from '@/context/LanguageContext'
 
-type SkillCardProps = {
-  icon: React.ReactNode
-  title: string
-  technologies: string[]
+type SkillCardBase = {
+  card: SkillCardType
   onClick?: () => void
 }
 
-export const SkillCard = ({ icon, title, technologies, onClick }: SkillCardProps) => {
+type SkillCardWithDetails = {
+  locale: Locale
+  showDetails: true
+}
+
+type SkillCardWithoutDetails = {
+  locale?: Locale
+  showDetails?: false
+}
+
+type SkillCardProps = SkillCardBase & (SkillCardWithDetails | SkillCardWithoutDetails)
+
+export const SkillCard = ({
+  card,
+  onClick,
+  locale = 'fr',
+  showDetails = false,
+}: SkillCardProps) => {
   return (
     <div
       className={clx(
         'bg-gray-dark dark:bg-gray-darkest dark:shadow-gray-darkest p-6 dark:border dark:border-gray-light',
-        'rounded-lg w-full sm:w-56 min-w-56 shadow-lg cursor-pointer'
+        'rounded-lg w-full min-w-56 shadow-lg',
+        !showDetails && 'sm:w-56 cursor-pointer'
       )}
       {...(onClick ? { onClick } : {})}
     >
-      <div className="mb-2">{icon}</div>
-      <h3 className="mb-2 font-semibold !text-gray-lightest text-lg">{title}</h3>
+      <div className="mb-2">{card.icon}</div>
+      <h3 className="mb-2 font-semibold !text-gray-lightest text-lg">{card.title}</h3>
       <p className="font-light !text-gray-light leading-relaxed">
-        {technologies.map((tech, index) => (
+        {card.technologies.map((tech, index) => (
           <span key={index}>
             {tech}
-            {index < technologies.length - 1 && ', '}
+            {index < card.technologies.length - 1 && ', '}
           </span>
         ))}
       </p>
+
+      <RenderIf condition={showDetails}>
+        <div className="mt-4">
+          <ul className="list-disc list-inside pl-2">
+            {card.details[locale].map((desc, index) => (
+              <li key={index}>{desc}</li>
+            ))}
+          </ul>
+        </div>
+      </RenderIf>
     </div>
   )
 }
