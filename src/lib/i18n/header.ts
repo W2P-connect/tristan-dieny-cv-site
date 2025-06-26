79f2417cc35a61aee17aa78b085750e5573a994a
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-06-26 14:45:07 +0200

    feat: add i18n label system
    Introduce structured folder for multilingual labels and future translations.

---
+++ src/lib/i18n/header.ts
@@ -0,0 +1,17 @@
+// lib/i18n/header.ts
+export const headerTranslations = {
+    en: {
+        name: "Tristan Dieny",
+        title: "Full-Stack Web Developer",
+        tagline: "Passionate about creating innovative web solutions",
+        description:
+            "Full-stack web developer with a passion for building robust and scalable web applications. With a strong foundation in both front-end and back-end technologies, I enjoy tackling complex challenges and delivering high-quality solutions that meet user needs.",
+    },
+    fr: {
+        name: "Tristan Dieny",
+        title: "Développeur Web Full-Stack",
+        tagline: "Passionné par la création de solutions web innovantes",
+        description:
+            "Développeur web full-stack passionné par la création d'applications web robustes et évolutives. Fort d'une solide expérience en front-end comme en back-end, j’aime relever des défis techniques et proposer des solutions de qualité adaptées aux besoins des utilisateurs.",
+    },
+}
