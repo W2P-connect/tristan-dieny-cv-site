955876237962b02f15b6375f12886ae89a1edbf3
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-06-27 11:07:38 +0200

    feat(neon-header): add social icons to header

--- src/components/_sections/home/NeonHeader.tsx
+++ src/components/_sections/home/NeonHeader.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image'
 import NeonHeaderContainer from '../../containers/neonHeaderContainer/NeonHeaderContainer'
 import Link from 'next/link'
-import { ArrowDownCircleIcon } from '@heroicons/react/24/outline'
+import { ArrowDownCircleIcon, ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline'
 import { headerTranslations } from '@/lib/i18n/header'
+import { socials } from '@/lib/socials'
 
 type NeonHeaderProps = {
   locale: 'en' | 'fr'
@@ -13,7 +14,7 @@ export const NeonHeader = ({ locale }: NeonHeaderProps) => {
 
   return (
     <NeonHeaderContainer>
-      <div>
+      <div className="">
         <div className="flex sm:flex-row flex-col gap-4">
           <div className="flex-shrink-0">
             <Image
@@ -31,9 +32,38 @@ export const NeonHeader = ({ locale }: NeonHeaderProps) => {
           </div>
         </div>
 
-        <p className="mt-12 font-medium text-gray-500 text-lg sm:text-xl/8 text-pretty">
+        <p className="my-12 font-medium text-gray-500 text-lg sm:text-xl/8 text-pretty">
           {t.description}
         </p>
+
+        <div className="flex flex-wrap justify-center items-center gap-8">
+          {socials.map((social, index) => (
+            <div key={index} className="flex flex-col items-center gap-2">
+              <div className="">
+                <Image
+                  className="rounded-full overflow-hidden"
+                  src={`/icons/social/${social.icon}`}
+                  alt={social.name}
+                  width={36}
+                  height={36}
+                />
+              </div>
+
+              <div>
+                <p className="font-semibold !text-gray-darker dark:!text-gray-lightest-2 small-text">
+                  {social.name}
+                </p>
+                <Link key={index} href={social.url} target="_blank" rel="noopener noreferrer">
+                  <div className="flex items-center gap-2 small-text">
+                    {social.userName}
+                    <ArrowTopRightOnSquareIcon width={12} />
+                  </div>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+
         <div className="flex justify-center items-center mt-24">
           <Link href={`/#skills`}>
             <ArrowDownCircleIcon className="animate-bounce" width={48} height={48} />
