9fe6d97335572a2ff8dbd775908e3da54b07a7bd
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-07-02 08:46:05 +0200

    feat(w2p): ajout de la page projet W2P en français et en anglais

--- src/app/en/projects/w2p/page.tsx
+++ src/app/en/projects/w2p/page.tsx
@@ -1,8 +1,6 @@
-import W2p from '@/components/_sections/projects/w2p'
+import W2p from '@/components/_sections/projects/W2p'
 import React from 'react'
 
 export default function page() {
-    return (
-        <W2p />
-    )
+  return <W2p />
 }
--- src/components/_sections/projects/W2p.tsx
+++ src/components/_sections/projects/W2p.tsx
@@ -1,13 +1,44 @@
 import MainHeaderContainer from '@/components/containers/mainContainer/MainContainer'
 import SectionContainer from '@/components/containers/sectionContainer/SectionContainer'
+import { Locale } from '@/context/LanguageContext'
+import { projects } from '@/lib/projects'
+import { headers } from 'next/headers'
 import React from 'react'
+import ProjectHeader from '@/components/containers/project/ProjectHeader'
+import { redirect } from 'next/navigation'
+import ProjectTools from '@/components/containers/project/ProjectTools'
+import { w2pProjectContent } from '@/lib/i18n/w2p'
+import ProjectSection from '@/components/containers/project/ProjectSection'
+import RenderIf from '@/components/containers/renderIf/RenderIf'
 
-export default function W2p() {
-    return (
-        <MainHeaderContainer bluredBackground>
-            <SectionContainer>
-                <h1>Woocommerce 2 Pipedrive</h1>
-            </SectionContainer>
-        </MainHeaderContainer>
-    )
+export default async function W2p() {
+  const header = await headers()
+  const locale = header.get('x-locale') as Locale
+
+  const project = projects.find((project) => project.slug === 'w2p')
+
+  if (!project) {
+    redirect('/not-found')
+  }
+
+  const content = w2pProjectContent[locale]
+
+  return (
+    <MainHeaderContainer bluredBackground>
+      <SectionContainer>
+        <ProjectHeader project={project} locale={locale} />
+
+        <div className="space-y-12 mt-16">
+          {Object.entries(content).map(([key, section]) => (
+            <div key={key}>
+              <ProjectSection section={section} sectionKey={key} />
+              <RenderIf condition={key === 'tech'}>
+                <ProjectTools projectTools={project.tools} />
+              </RenderIf>
+            </div>
+          ))}
+        </div>
+      </SectionContainer>
+    </MainHeaderContainer>
+  )
 }
