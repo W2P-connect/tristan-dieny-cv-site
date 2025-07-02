0d6c364491287a782e0eef98804df610192f985b
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-07-02 09:08:51 +0200

    💄 style: update formatting in page.tsx, Llfc.tsx, and Spotlight.tsx for consistency

--- src/components/_sections/projects/Llfc.tsx
+++ src/components/_sections/projects/Llfc.tsx
@@ -2,13 +2,12 @@ import MainHeaderContainer from '@/components/containers/mainContainer/MainConta
 import SectionContainer from '@/components/containers/sectionContainer/SectionContainer'
 import React from 'react'
 
-
 export default function Llfc() {
-    return (
-        <MainHeaderContainer bluredBackground>
-            <SectionContainer>
-                <h1>LLFC</h1>
-            </SectionContainer>
-        </MainHeaderContainer>
-    )
+  return (
+    <MainHeaderContainer bluredBackground>
+      <SectionContainer>
+        <h1>LLFC</h1>
+      </SectionContainer>
+    </MainHeaderContainer>
+  )
 }
--- src/components/_sections/projects/Spotlight.tsx
+++ src/components/_sections/projects/Spotlight.tsx
@@ -2,13 +2,12 @@ import MainHeaderContainer from '@/components/containers/mainContainer/MainConta
 import SectionContainer from '@/components/containers/sectionContainer/SectionContainer'
 import React from 'react'
 
-
 export default function Spotlight() {
-    return (
-        <MainHeaderContainer bluredBackground>
-            <SectionContainer>
-                <h1>Spotlight</h1>
-            </SectionContainer>
-        </MainHeaderContainer>
-    )
+  return (
+    <MainHeaderContainer bluredBackground>
+      <SectionContainer>
+        <h1>Spotlight</h1>
+      </SectionContainer>
+    </MainHeaderContainer>
+  )
 }
