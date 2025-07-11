415b07e4056fac3651b6430a80ba7b5ab174bf00
W2P-connect <woocomerce2pipedrive@gmail.com>
2025-07-11 14:22:18 +0200

    🚑 fix: update CustomLink to handle opening links in new tab

--- src/components/general/customLink/CustomLink.tsx
+++ src/components/general/customLink/CustomLink.tsx
@@ -5,13 +5,14 @@ import Link from 'next/link'
 
 type Props = {
   href: string
+  newTab?: boolean
   children: React.ReactNode | string
 }
 
-const CustomLink = async ({ href, children }: Props) => {
+const CustomLink = async ({ href, children, newTab }: Props) => {
   const header = await headers()
   const locale = header.get('x-locale') as Locale
-  return <Link href={`/${locale}${href}`}>{children}</Link>
+  return <Link target={newTab ? '_blank' : '_self'} href={`/${locale}${href}`}>{children}</Link>
 }
 
 export default CustomLink
